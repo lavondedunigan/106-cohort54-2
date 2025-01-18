8e72ea6ba39bdc2026d45428c5454929c6657018
lavondedunigan <lavondedunigan05@gmail.com>
2025-01-18 13:45:51 -0600

    106-2

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+function saveTask()
+{
+    console.log('saving task');
+    //get values
+
+    //build an object
+
+    //save to server
+
+    //display the data received from server
+}
+
+function init(){
+    console.log('init');
+    //load data
+
+    // hook events
+    $("#btnSave").click(saveTask);
+}
+
+window.onload = init;
+//Variable Scope
---
+++ scripts/example.js
@@ -0,0 +1,16 @@
+function sayHello(){
+    console.log("Hello");
+}
+function init(){
+
+    console.log("Hello world");
+    sayHello();
+    sayHi("Vonda");
+}
+function sayHi(){
+    console.log("hi"+name)
+}
+
+
+window.onload - init;
+//Variable scope
