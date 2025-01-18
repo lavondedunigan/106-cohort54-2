b7660afd9d8cecaa7e0a7dd3f37e7997d2fca864
lavondedunigan <lavondedunigan05@gmail.com>
2025-01-18 13:46:15 -0600

    106-2

--- scripts/app.js
+++ scripts/app.js
@@ -2,14 +2,51 @@ function saveTask()
 {
     console.log('saving task');
     //get values
+    const title = $("#txtTitle").val();
+    const description = $("#txtDescription").val();
+    const color = $("#txtselColor").val();
+    const date= $("#selDate").val();
+    const status = $("#selStatus").val();
+    const number = $("#numBudget").val();
+    console.log(title,description,color,date,status,number);
 
     //build an object
-
+    let taskToSave = new Task (title,description,color,date,status,number)
+    console.log(taskToSave);
     //save to server
 
     //display the data received from server
+    displayTask(taskToSave);
 }
+function displayTask(task)
+ {      
+    let syntax = `<div class='task'>
+    <div class='info'>
+        <h5>${task.title}</h5>
+        <p>${task.description}</p>
+    </div>
+    <label class='status'>${task.status}</label>
+    <div class='date-budget'>
+    <label>${task.date}</label>
+    <label>${task.budget}</label>
+    </div>
+    </div>
+    `;
+    $(".pending.tasks").append(syntax);
+ }
 
+ function testFunction()
+ {
+    $.ajax({
+        type: "get",
+        url:"http://fsdiapi.azurewebsites.net",
+        success: function(response)
+        {
+            console.log(response);
+        },
+        error: function(error){}
+    });
+ }
 function init(){
     console.log('init');
     //load data
---
+++ scripts/task.js
@@ -0,0 +1,13 @@
+class Task {
+
+    constructor(title, desc, color, data, status, number)
+    {
+        this.title = title;
+        this.description = description;
+        this.color = color;
+        this.date = date;
+        this.status = status;
+        this.budget = budget;
+    }
+
+}
